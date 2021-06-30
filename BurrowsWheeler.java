e7c13834b2342808b047c1c66e9af363600b8fe5
vastera <vastear@163.com>
2021-06-30 22:26:49 +0800

    unfinished inverse-Transform

---
+++ BurrowsWheeler.java
@@ -0,0 +1,64 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.Arrays;
+
+public class BurrowsWheeler {
+    private static int first;
+    private static int[] t;
+
+    // apply Burrows-Wheeler transform,
+    // reading from standard input and writing to standard output
+    public static void transform() {
+        String s = BinaryStdIn.readString();
+        CircularSuffixArray csa = new CircularSuffixArray(s);
+        int[] t = new int[csa.length()];
+        for (int i = 0; i < csa.length(); i++) {
+            if (csa.index(i) == 0) {
+                t[i] = csa.length() - 1;
+                first = i;
+            }
+            else
+                t[i] = csa.index(i) - 1;
+        }
+        BinaryStdOut.write(first);
+        for (int i = 0; i < t.length; i++) {
+            BinaryStdOut.write(s.charAt(t[i]));
+        }
+        BinaryStdOut.close();
+    }
+
+    // apply Burrows-Wheeler inverse transform,
+    // reading from standard input and writing to standard output
+    public static void inverseTransform() {
+        int[] firstCol = new int[t.length];
+        for (int i = 0; i < firstCol.length; i++)
+            firstCol[i] = t[i];
+        Arrays.sort(firstCol);
+        int[] next = new int[firstCol.length];
+        boolean[] mark = new boolean[firstCol.length];
+        for (int i = 0; i < firstCol.length; i++) {
+            for (int j = 0; j < t.length; j++) {
+                if (!mark[j] && firstCol[i] == t[j]) {
+                    next[i] = j;
+                    mark[j] = true;
+                }
+            }
+        }
+        int cur = first;
+        do {
+            BinaryStdOut.write(firstCol[cur]);
+            cur = next[cur];
+        } while (cur != 0);
+
+    }
+
+    // if args[0] is "-", apply Burrows-Wheeler transform
+    // if args[0] is "+", apply Burrows-Wheeler inverse transform
+    public static void main(String[] args) {
+        if (args[0].equals("-")) transform();
+        else if (args[0].equals("+")) inverseTransform();
+        else
+            throw new IllegalArgumentException("input must be '-' or '+'!");
+    }
+}
